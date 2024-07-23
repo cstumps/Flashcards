b7b155d9b682ad5d92d171242b6f75e8fe8b90b5
Jason Hirst <jhirst@hvc.rr.com>
2024-07-22 21:21:39 -0400

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Jason Hirst on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
